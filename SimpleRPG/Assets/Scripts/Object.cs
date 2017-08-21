f8c7c2b037c46af37dcbc19267053b9e10797ef0
reverinu <reverinudog@gmail.com>
2017-08-21 21:54:32 +0900

    【内容】追加　【作業理由】Object基底クラス作成

---
+++ SimpleRPG/Assets/Scripts/Object.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * オブジェクト基底クラス
+ */
+public class Object {
+
+    /* 名前 */
+    [SerializeField] private string name;
+	
+}
