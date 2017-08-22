aa109037ef6f84952edfb2c923b1c9a5ee229578
reverinu <reverinudog@gmail.com>
2017-08-22 14:07:35 +0900

    【内容】修正　【作業理由】PlayerController実装

---
+++ SimpleRPG/Assets/Scripts/Controller/ReverPlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReverPlayerController : MonoBehaviour
+{
+    public float Speed = 10f;
+    private Rigidbody rb;
+    // Use this for initialization
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate(){
+        float moveHorizontal = Input.GetAxisRaw( "Horizontal" );
+        float moveVertical = Input.GetAxisRaw( "Vertical" );
+
+        Vector3 movement = new Vector3( moveHorizontal, 0f, moveVertical );
+        rb.AddForce( movement * Speed );
+    }
+}
