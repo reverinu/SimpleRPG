a38f357ebf9b2384fda0e4a1b8f0ad8576fa9f00
reverinu <reverinudog@gmail.com>
2017-08-22 20:10:45 +0900

    【内容】修正　【作業理由】プレハブの修正（Playerにカメラを内蔵）

--- SimpleRPG/Assets/Scripts/Manager/ReverGameManager.cs
+++ SimpleRPG/Assets/Scripts/Manager/ReverGameManager.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class ReverGameManager : MonoBehaviour {
 
+    public static GameObject Player;
+    public static GameObject[] Ornaments;
+    
+
 	// Use this for initialization
 	void Start () {
-		
+		// 初期化
+        // オブジェクト配置
+        // スタートするものはスタートさせておく
 	}
 	
 	// Update is called once per frame
