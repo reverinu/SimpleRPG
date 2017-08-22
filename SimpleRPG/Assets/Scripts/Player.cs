ef951564e5ff58a92d08fa261e033c40565ed9ad
reverinu <reverinudog@gmail.com>
2017-08-22 12:32:25 +0900

    【内容】修正　【作業理由】Player.prefabの修正

---
+++ SimpleRPG/Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * プレイヤークラス
+ */
+public class Player : Character {
+    [SerializeField] private int Hp;
+    [SerializeField] private int Mp;
+    [SerializeField] private int Atk;
+    [SerializeField] private int Def;
+}
